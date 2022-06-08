b4b5e15f7fc4b2db21a42b50ba783df45b1e1fb9
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-08 13:26:52 -0300

    Read stream data from connection attempt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +7,18 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+
+fn handle_connection(mut stream: TcpStream) {
+    // buffer big enough to read contents of stream
+    // if we needed to read arbitrary-sized streams, buffer management
+    // would be more complicated
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
