9f610b7763d27653884e306d3db6271ad8f220c4
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-08 15:48:43 -0300

    Add sleep to simulate a slow request

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::fs;
-use std::fs::read_to_string;
+use std::{fs, thread};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -22,8 +23,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get_request_text = b"GET / HTTP/1.1\r\n";
+    let sleep_request_text = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get_request_text) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep_request_text) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
